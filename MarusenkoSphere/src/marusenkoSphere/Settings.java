6bc025cf57106b35772596edd091a5ed080a0516
fadnincx <fadnincx@gmail.com>
2014-08-09 11:22:37 +0200

    Now with Settings

---
+++ MarusenkoSphere/src/marusenkoSphere/Settings.java
@@ -0,0 +1,19 @@
+package marusenkoSphere;
+
+/**
+ * Settings-Klasse
+ * Enthält diverse Einstellungen an einem Zentralen Ort, 
+ * so dass diese nicht überall zusammen gesucht werden müssen
+ *
+ */
+public class Settings {
+	
+	//Allgemein
+	public static final boolean DEBUGMODE = true;
+	
+	//Main
+	public static final boolean STARTKONSOLE = false;
+
+	//RendernKugel
+	public static final int KUGELRENDERRASTER = 10; //Nur 1, 2, 5, 10
+}
